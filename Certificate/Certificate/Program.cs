252e28161f87812ef1fd85af4343504f085c294e
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-09-12 00:54:58 +0300

    cert

--- Certificate/Certificate/Program.cs
+++ Certificate/Certificate/Program.cs
@@ -6,7 +6,23 @@
         {
             // Sablonas microsoft sertifikatui
 
+            int dayOfWeek = 3;
+            string dayName;
 
+            switch (dayOfWeek)
+            {
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                default:
+                    dayName = "Unknown";
+                    break;
+            }
+
+            Console.WriteLine(dayName);
         }
     }
 }
